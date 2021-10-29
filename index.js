40fcc6888e2d1ae7eedeb088b2755e76434d7b97
Md Mahmudul Hasan <92252013+mdmhn@users.noreply.github.com>
2021-10-29 15:52:52 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage1 = "images/dice" + randomNumber1 + ".png";
+
+document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage1);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
+
+if (randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML="🏆 Play 1 Wins!";
+}
+else if (randomNumber1 < randomNumber2){
+  document.querySelector("h1").innerHTML="Play 2 Wins! 🏆";
+}
+else {
+    document.querySelector("h1").innerHTML = "Draw! 😞";
+}
